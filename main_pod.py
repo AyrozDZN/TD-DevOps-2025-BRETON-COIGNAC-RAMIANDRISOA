e03b21dea45870ef6001e54887745d067ab406a5
Thibaut Breton <contact@tbreton.fr>
2025-01-09 14:05:08 +0100

    Correction linter

--- main_pod.py
+++ main_pod.py
@@ -1,14 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    pod_name = os.getenv('POD_NAME', 'Unknown Pod')
-    #return f'This is the pod: {pod_name}'
-    #return f'Hello'
-    return f'''
+    pod_name = os.getenv("POD_NAME", "Unknown Pod")
+    return f"""
     <html>
         <head>
             <title>Pod Info</title>
@@ -17,8 +15,8 @@ def index():
             <h1>Hello, the pod name is {pod_name}</h1>
         </body>
     </html>
-    '''
+    """
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
     
