0135daa3406ce251b0f301dff59d354d6baa8b30
Thibaut Breton <contact@tbreton.fr>
2025-01-16 13:43:23 +0100

    app_red with CAT

--- main_pod.py
+++ main_pod.py
@@ -8,14 +8,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     pod_name = os.getenv("POD_NAME", "Unknown Pod")
+    script = """
+    fetch("https://api.thecatapi.com/v1/images/search").then(response => response.json()).then(data => {
+        document.getElementById("cat").src = data[0].url;
+    }).catch(error => {
+        console.error("Error fetching cat image:", error);
+    });
+    """
+    
     return f"""
     <html>
         <head>
             <title>Pod Info</title>
         </head>
-        <body style="background-color: green; color: white; text-align: center; padding-top: 20%;">
+        <body style="background-color: red; color: white; text-align: center; padding-top: 20%;">
             <h1>Hello, the pod name is {pod_name}</h1>
+            <img src="" alt="" id="cat">
         </body>
+        <script>
+            {script}
+        </script>
     </html>
     """
 
